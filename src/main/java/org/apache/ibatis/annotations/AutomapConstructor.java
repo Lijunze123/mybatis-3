08d6ad46aedb1c3b40c7cff336673c588ff563d8
jerry <leejay520@foxmail.com>
2018-08-22 22:24:26 +0800

    commit auto

--- src/main/java/org/apache/ibatis/annotations/AutomapConstructor.java
+++ src/main/java/org/apache/ibatis/annotations/AutomapConstructor.java
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
 /**
  * The marker annotation that indicate a constructor for automatic mapping.
  *
- * @author Tim Chen
+ * @author Jerry Li
  * @since 3.4.3
  */
 @Documented
